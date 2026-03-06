f274f593024801ad9561319468f7e140b09b0374
Unam Matheatau <matheatauunnam@gmail.com>
2026-03-06 17:02:32 +0200

    Add backend folder files

---
+++ ledger-backend/server.js
@@ -0,0 +1,26 @@
+import express, { json } from "express";
+import { connect } from "mongoose";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+// eslint-disable-next-line no-undef
+require("dotenv").config();
+
+const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(json());
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+
+// eslint-disable-next-line no-undef
+connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"));
+
+// Routes
+// eslint-disable-next-line no-undef
+app.use("/api/auth", require("./routes/auth"));
+// eslint-disable-next-line no-undef
+app.use("/api/expenses", require("./routes/expenses"));
+// eslint-disable-next-line no-undef
+app.use("/api/budgets", require("./routes/budgets"));
+// eslint-disable-next-line no-undef
+app.use("/api/accounts", require("./routes/accounts"));
+
+app.listen(5000, () => console.log("Server on port 5000"));
